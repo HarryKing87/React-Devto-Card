89c6cec4a4aa3cb1bc624f3bc9991dc596269032
Harry Kinigopoulos <46583043+HarryKing87@users.noreply.github.com>
2020-09-14 03:37:03 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from "react";
+  import Card from "./card";
+
+  function App(){
+    return (
+      <div className="wrapper">
+        <Card />
+      </div>
+    );
+  }
+
+  export default App;
